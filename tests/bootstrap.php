95c7b69b81b211d94eb61a75ed96e9ce2cfa80a0
Kevin Andrews <kevin@nforced.co.uk>
2014-01-14 02:10:36 +0000

    Removed tests, impossible to run locally without entire zpanel framework installed.

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../../dryden/loader.inc.php';
-require_once __DIR__ . '/../../../dryden/debug/phperrors.class.php';
-require_once __DIR__ . '/../../../dryden/debug/logger.class.php';
-require_once __DIR__ . '/../../../dryden/db/driver.class.php';
-require_once '/../../../cnf/db.php';
-debug_phperrors::SetMode( 'dev' );
-require_once '/../../../inc/dbc.inc.php';
